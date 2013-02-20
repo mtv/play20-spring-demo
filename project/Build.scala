a4a415c8476b65ed69ea6fd9009d42f2f8e993b9
Morten Tuft Vareberg <movar@statoil.com>
2013-02-20 12:41:15 +0100

    Upgraded play version to 2.1.0 and spring version to 3.2.1.RELEASE

--- project/Build.scala
+++ project/Build.scala
@@ -5,14 +5,17 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
     val appName         = "play-spring"
-    val appVersion      = "1.0-SNAPSHOT"
+    val appVersion      = "1.0.0-SNAPSHOT" // Semantic Versioning - http://semver.org/
 
-    val appDependencies = Seq(
-      "org.springframework" % "spring-context" % "3.0.7.RELEASE"
-    )
+	val appDependencies = Seq(
+		javaCore,
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      // Add your own project settings here      
-    )
+		// Spring dependency
+		"org.springframework" % "spring-context" % "3.2.1.RELEASE"
+	)
+
+	val main = play.Project(appName, appVersion, appDependencies).settings(
+		// Add your own project settings here      
+	)
 
 }
